86164c70a0b367cf21fed00dc6d07c0a1834f34d
brecht tanghe <brecht_tanghe@hotmail.com>
2017-02-08 17:35:26 +0100

    finish nodejs course

--- npmProject/app.js
+++ npmProject/app.js
@@ -1,12 +1,13 @@
 var express = require('express');
 var app = express();
+var mongoose = require('mongoose');
+
+mongoose.connect('');
 
 var apiController = require('./controllers/apiController');
 var htmlController = require('./controllers/htmlController');
 
 var port = process.env.PORT || 3000;
-
-
 
 app.use('/assets', express.static(__dirname + '/public'));
 
